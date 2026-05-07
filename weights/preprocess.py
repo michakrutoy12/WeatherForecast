978e8e3cdb270952b1f9fbb728ca0f2eb1a0ae7d
Micha <micha_krutoy@icloud.com>
2026-05-07 22:25:03 +0700

    New model

--- weights/preprocess.py
+++ weights/preprocess.py
@@ -1,175 +1,125 @@
-import os
-import pickle
+import pandas as pd
 import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
+import pickle
+import glob
+import os
 import re
-from datetime import datetime
 
-columns = ('hour_sin', 'hour_cos', 'day_sin', 'day_cos', 'precipation', 'pressure', 'pressure_trend', 'temperature', 'humidity', 'wind_speed')
-HISTORY_HOURS = 24 * 5
-PREDICTION_HOURS = 24 * 5
-SAMPLING_INTERVAL = 4
-W_epsilon = 1
+train_columns = ['T', 'P0', 'Dp']
+target_columns = ['T', 'Pr']
+HISTORY_LEN = 24
+PREDICTION_LEN = 24
+TRAIN_SPLIT = 0.9
 
-targets = [x for x in os.listdir('dataset') if x.split('.')[-1] == 'csv']
-
-total_read = []	
-
-for target in targets:
-	with open("dataset/" + target, 'r', encoding='UTF-8') as file:
-		for line in file.readlines()[::-1]:
-			line = line.replace('"', '').split(';')
-			if line[0].split(' ')[1].split(':')[1] not in ('00', '30'):
-				continue
-			if line[1] == '' or line[2] == '':
-				continue
-			total_read.append({
-				'date': line[0],
-				'temperature': line[1],
-				'sea_level_pressure': line[2],
-				'pressure': line[3],
-				'humidity': line[4] or 0,
-				'wind': line[5],
-				'wind_speed': line[6],
-				'max_wind_speed': line[7] or 0,
-				'weather': line[8],
-				'cloud_shape': line[10],
-				'horizontal_visibility': line[11],
-				'dew_point_temperature': line[12]
-			})
-		file.close()
-
-# w = set()
-# for x in total_read:
-# 	for z in x['weather'].split(','):
-# 		if z:
-# 			w.add(re.sub(r'\s*\([^)]*\)', '', z.strip().lower()).replace(')', '').replace('(', ''))
-
-# print(w, len(w))
-
-features = { ## Оценка осадков от 1 до 10
-	'ливень': 7,
-	'сильный ливень': 9,
-	'вблизи гроза': 5,
-	'небольшой град и/или снежная крупа': 6,
-	'сильный снег': 8, 
-	'буря снег': 10,
-	'слабый ливень': 4,
-	'град': 9,
-	'слабый снег': 3,
-	'дождь': 6,
-	'слабый гроза': 4,
-	'вблизи ливень': 3,
-	'морось': 2,
-	'гроза': 8,
-	'слабый морось': 1,
-	'слабый дождь': 3,
-	'сильный гроза': 10,
-	'слабый замерзающий дождь': 8,
-	'снег': 5
+precipitation_map = {
+    'ливень': 8, 'сильный ливень': 9, 'вблизи гроза': 5,
+    'небольшой град и/или снежная крупа': 8, 'сильный снег': 8, 
+    'буря снег': 10, 'слабый ливень': 4, 'град': 9,
+    'слабый снег': 3, 'дождь': 7, 'слабый гроза': 6,
+    'вблизи ливень': 3, 'морось': 4, 'гроза': 8,
+    'слабый морось': 3, 'слабый дождь': 5, 'сильный гроза': 10,
+    'слабый замерзающий дождь': 8, 'снег': 7
 }
 
-clear_weather = lambda s: re.sub(r'\s*\([^)]*\)', '', s.strip().lower()).replace(')', '').replace('(', '')
+def score_weather(weather_string):
+    if pd.isna(weather_string) or weather_string == '':
+        return 0.0
+    parts = str(weather_string).lower().split(',')
+    scores = []
+    for p in parts:
+        clean_p = re.sub(r'\s*\([^)]*\)', '', p).strip().replace(')', '').replace('(', '')
+        scores.append(precipitation_map.get(clean_p, 0))
+    output = (sum(scores) / len(scores)) / 10 if scores else 0.0
+    return 1 if output > 0 else 0
 
-data = {}
+all_files = glob.glob(os.path.join('dataset', "*.csv"))
+dataframes = []
 
-P = [float(x['sea_level_pressure']) for x in total_read]
-T = [float(x['temperature']) for x in total_read]
-P_mean, P_std = np.mean(P), np.std(P)
-T_mean, T_std = np.mean(T), np.std(T)
+for filename in all_files:
+    try:
+        df = pd.read_csv(filename, sep=';', encoding='utf-8', quotechar='"', skiprows=1, index_col=False)
+        dataframes.append(df)
+    except Exception as e:
+        print(f"Ошибка чтения файла {filename}: {e}")
 
-W = np.log(np.array([float(x['wind_speed']) for x in total_read]) + W_epsilon)
-W_mean, W_std = np.mean(W), np.std(W)
+full_df = pd.concat(dataframes, ignore_index=True)
 
-p_hist = [-1 for i in range(SAMPLING_INTERVAL)]
-p_a = []
+full_df['D'] = pd.to_datetime(full_df['D'], dayfirst=True)
+full_df = full_df.sort_values(by='D').set_index('D')
 
-for x in total_read:
-	line = {}
-	## Sin/Cos Date/Time Encoding
-	dt = datetime.strptime(x['date'], "%d.%m.%Y %H:%M")
-	line['hour_sin'] = np.sin(2 * np.pi * dt.hour / 24)
-	line['hour_cos'] = np.cos(2 * np.pi * dt.hour / 24)
-	line['day_sin'] = np.sin(2 * np.pi * dt.timetuple().tm_yday / 365.25)
-	line['day_cos'] = np.cos(2 * np.pi * dt.timetuple().tm_yday / 365.25)
+full_df['T'] = full_df['T'].rolling(window=6, min_periods=1).mean()
+full_df['Pr'] = full_df['Pr'].apply(score_weather)
 
-	## Precipitation classification
-	w = [features.get(clear_weather(a), 0) / 10 for a in x['weather'].split(',')]
-	line['precipation'] = sum(w) / len(w)
+a = 17.27
+b = 237.7
+# full_df['H'] = 100 * np.exp((a * full_df['Dp']) / (b + full_df['Dp'])) / np.exp((a * full_df['T']) / (b + full_df['T']))
+# full_df['Dp'] = full_df['H'].round(2)
 
-	## Temperature, Pressure and Humidity normalization
-	line['pressure'] = (float(x['sea_level_pressure']) - P_mean) / P_std
-	p_hist = p_hist[1:] + [line['pressure']]
+columns_to_keep = list(set(train_columns + target_columns))
+full_df = full_df[columns_to_keep].resample('1h').mean()
+dataset = full_df.interpolate(method='linear')
 
-	if -1 in p_hist:
-		line['pressure_trend'] = 0.0
-	else:
-		line['pressure_trend'] = float(p_hist[-1] - p_hist[0]) * P_std
-		p_a.append(line['pressure_trend'])
+train_size = int(len(dataset) * TRAIN_SPLIT)
+train_data = dataset.iloc[:train_size]
 
-	line['temperature'] = (float(x['temperature']) - T_mean) / T_std
-	line['humidity'] = int(x['humidity']) / 100 - 0.5
+stats = {}
+for col in train_columns:
+    if col == 'Pr': continue
+    
+    mean = train_data[col].mean()
+    std = train_data[col].std()
+    
+    if std == 0: std = 1e-6 
+    
+    dataset[col] = (dataset[col] - mean) / std
+    stats[col] = {'mean': mean, 'std': std}
 
-	## Wind Speed normalization
-	line['wind_speed'] = (np.log(float(x['wind_speed']) + W_epsilon) - W_mean) / W_std
+inputs = dataset[train_columns].values
+targets = dataset[target_columns].values
 
-	data[int(dt.timestamp())] = line
+x_windows = sliding_window_view(inputs, window_shape=(HISTORY_LEN, inputs.shape[1]))
+x_windows = x_windows[:-(PREDICTION_LEN), 0] 
 
-Pt_mean = np.mean(p_a)
-Pt_std = np.std(p_a)
+y_windows = sliding_window_view(targets, window_shape=(PREDICTION_LEN, targets.shape[1]))
+y_windows = y_windows[HISTORY_LEN:, 0]
 
-for timestamp in data:
-	data[timestamp]['pressure_trend'] = (data[timestamp]['pressure_trend'] - Pt_mean) / Pt_std
+X = x_windows.transpose(0, 2, 1)
+Y = y_windows.transpose(0, 2, 1)
 
-train = []
-targets = []
 
-def clear_data(data):
-	return np.array([data[c] for c in columns])
+# output = {
+#     'train_columns': train_columns,
+#     'target_columns': target_columns,
+#     'train': {
+#         'X': X[:train_size],
+#         'y_temp': Y[:train_size, 0, :],
+#         'y_precip': Y[:train_size, 1, :]
+#     },
+#     'test': {
+#         'X': X[train_size:],
+#         'y_temp': Y[train_size:, 0, :],
+#         'y_precip': Y[train_size:, 1, :]
+#     },
+#     'stats': stats,
+#     'history_length': HISTORY_LEN,
+#     'prediction_length': PREDICTION_LEN,
+#     'train_split': TRAIN_SPLIT
+# }
 
-for i in range(min(data) + 3600*HISTORY_HOURS, max(data) - 3600*PREDICTION_HOURS, 3600):
-	n = 0
-	train_sample = []
-	target_sample = []
-
-	for j in range(i - 3600*HISTORY_HOURS, i, 3600 * SAMPLING_INTERVAL):
-		if not j in data:
-			n += 1
-		else:
-			train_sample.append(clear_data(data[j]))
-
-	for j in range(i, i + 3600*PREDICTION_HOURS, 3600 * SAMPLING_INTERVAL):
-		if not j in data:
-			n += 1
-		else:
-			target_sample.append(clear_data(data[j]))
-
-	if n == 0:
-		train.append(train_sample)
-		targets.append(target_sample)
-
-complete_data = {
-	'columns': columns,
-
-	'train': np.array(train, dtype=np.float32),
-	'targets': np.array(targets, dtype=np.float32),
-
-	'P_mean': P_mean,
-	'P_std': P_std,
-
-	'Pt_mean': Pt_mean,
-	'Pt_std': Pt_std,
-
-	'T_mean': T_mean,
-	'T_std': T_std,
-
-	'W_mean': W_mean,
-	'W_std': W_std,
-	'W_epsilon': W_epsilon,
-
-	'history_lenght': HISTORY_HOURS,
-	'prediction_lenght': PREDICTION_HOURS,
-	'sampling_interval': SAMPLING_INTERVAL
+output = {
+    'X': X,
+    'y': Y[:, 0, :],
+    'y_precip': Y[:, 1, :],
+    'T_mean': stats['T']['mean'],
+    'T_std': stats['T']['std'],
+    'U_mean': stats['Dp']['mean'],
+    'U_std': stats['Dp']['std'],
+    'P_mean': stats['P0']['mean'],
+    'P_std': stats['P0']['std']
 }
 
-with open('dataset/prepared.pkl', 'wb') as file:
-	pickle.dump(complete_data, file)
+with open('datasetGRU.pkl', 'wb') as file:
+    pickle.dump(output, file)
+
+print(f"Dataset prepared. Splitted at index {train_size}/{len(dataset)}")
